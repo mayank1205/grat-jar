7e540df2c258863fe1a163b2eba6f21611e90330
Mayank Joshi <mj.mayank1205@gmail.com>
2026-01-12 17:37:03 +0530

    fix posts APIs

--- src/main/java/com/gratjar/notes/controller/PostController.java
+++ src/main/java/com/gratjar/notes/controller/PostController.java
@@ -1,6 +1,8 @@
 package com.gratjar.notes.controller;
 
+import com.gratjar.notes.PostMapper;
 import com.gratjar.notes.entity.Post;
+import com.gratjar.notes.model.PostDTO;
 import com.gratjar.notes.service.PostService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,19 +20,19 @@ public class PostController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Post>> getAllPosts() {
-        return ResponseEntity.ok(postService.getAllPosts());
+    public ResponseEntity<List<PostDTO>> getAllPosts() {
+        return ResponseEntity.ok(postService.getAllPosts().stream().map(post -> PostMapper.toPostDTO(post)).toList());
     }
 
     @PostMapping
-    public ResponseEntity<Post> createPost(@RequestBody Post post) {
+    public ResponseEntity<PostDTO> createPost(@RequestBody Post post) {
         Post savedPost = postService.createPost(post);
-        return ResponseEntity.status(201).body(savedPost);
+        return ResponseEntity.status(201).body(PostMapper.toPostDTO(savedPost));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Post> getPost(@PathVariable Long id) {
-        return postService.getPostById(id)
+    public ResponseEntity<PostDTO> getPost(@PathVariable Long id) {
+        return postService.getPostById(id).map(post-> PostMapper.toPostDTO(post))
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
